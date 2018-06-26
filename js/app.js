adff80c3ef70841cfeae809c62b605b61c922dd2
Tyler Stahl <tylerstahl@gmail.com>
2018-06-26 15:31:54 -0400

    feat: Enemies move at variable speeds

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x,y, speed = Math.floor(Math.random() * 5 + 1 )) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -11,6 +11,8 @@ var Enemy = function(x,y) {
     //establishing original coordinates at x, y
     this.x = x;
     this.y = y;
+    //generate random speed 1-20 (Math.floor() and Math.random() principles learned from https://www.w3schools.com/jsref/jsref_random.asp)
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -19,6 +21,15 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    //mycode
+    //enemy movement, variable speed
+    this.x += this.speed * dt * 10;
+    //when enemy reaches right side of the board, reset to starting position
+    if (this.x > 400) {
+        this.reset();
+        this.speed = Math.floor(Math.random() * 200 + 1 );
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -42,14 +53,11 @@ var Player = function(x,y) {
 };
 Player.prototype.update = function() {
     //mycode
-    //collision detection
-
-    //restrict player to game board
-    
+    //collision detection    
     //win condition
     if (this.y < 40) {
         console.log("You win!");
-        player.reset();
+        this.reset();
     }
 };
 Player.prototype.render = function() {
@@ -58,6 +66,7 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(key) {
     if (key === "up") {
         this.y -= 85;
+    //restrict player to game board with && conditionals
     } else if ((key === "left") && (this.x > 0)) {
         this.x -= 100;
     } else if ((key === "down") && (this.y < 400)) {
